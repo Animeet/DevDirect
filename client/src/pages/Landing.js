43e1b7c91ca6e22aad9415f7d6107c555362461f
Zachary Barnes <zachhsavage@gmail.com>
2023-05-13 22:42:48 -0400

    Created Header and Footer | Still need to add logos

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+function Landing() {
+    return (
+        <main>
+            <Header />
+            <h1>Landing</h1>
+            <Footer />
+        </main>
+    );
+}
+
+export default Landing;
