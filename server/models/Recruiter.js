d3207738bf498eb13291c5871a5b6298a74d6993
jamessciacca <noing2003@gmail.com>
2023-05-15 20:43:46 -0400

    changed schema error

--- server/models/Recruiter.js
+++ server/models/Recruiter.js
@@ -50,14 +50,14 @@ const recruiterSchema = new Schema({
   });
 
   //removing the users password from the response
-userSchema.methods.toJSON = function () {
+recruiterSchema.methods.toJSON = function () {
     const user = this.toObject();
     delete user.password;
     return user;
 };
 
 //function to make password salty
-userSchema.pre('save', async function (next) {
+recruiterSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
       const encrypted_password = await bcrypt.hash(this.password, 10);
   
@@ -69,7 +69,7 @@ userSchema.pre('save', async function (next) {
   });
 
 //function to check if password is correct
-userSchema.methods.validatePass = async function (form_password) {
+recruiterSchema.methods.validatePass = async function (form_password) {
     const is_valid = await bcrypt.compare(form_password, this.password);
   
     return is_valid;
