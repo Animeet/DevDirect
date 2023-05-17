cd5528884878a52d803081db159985db2d8a27a3
MegMathis <mcdonough.megan25@gmail.com>
2023-05-17 15:10:02 -0400

    stuff with JD

--- server/models/User.js
+++ server/models/User.js
@@ -1,66 +1,72 @@
 //pulling in packages
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 //function to validate user email
-function validateEmail () {
-    const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    return emailRegex.test(this.email);
+function validateEmail() {
+  const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailRegex.test(this.email);
 }
 
 //creating user schema
 const userSchema = new Schema({
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: [validateEmail, 'You must enter a valid email address.']
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, "You must enter a valid email address."],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  portfolios: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "portfolio",
     },
-    password: {
-      type: String,
-      required: true,
-      min: 6
-    },
-    first_name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    last_name: {
-        type: String,
-        require: true,
-        trim: true
-    },
-  });
+  ],
+});
 
 //removing the users password from the response
 userSchema.methods.toJSON = function () {
-    const user = this.toObject();
-    delete user.password;
-    return user;
+  const user = this.toObject();
+  delete user.password;
+  return user;
 };
 
 //function to make password salty
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-      const encrypted_password = await bcrypt.hash(this.password, 10);
-  
-      this.password = encrypted_password;
-      return next();
-    }
-  
-    next();
-  });
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const encrypted_password = await bcrypt.hash(this.password, 10);
+
+    this.password = encrypted_password;
+    return next();
+  }
+
+  next();
+});
 
 //function to check if password is correct
 userSchema.methods.validatePass = async function (form_password) {
-    const is_valid = await bcrypt.compare(form_password, this.password);
-  
-    return is_valid;
+  const is_valid = await bcrypt.compare(form_password, this.password);
+
+  return is_valid;
 };
 
-//creating the MODEL 
-const User = model('user', userSchema);
+//creating the MODEL
+const User = model("user", userSchema);
 
 //exporting the MODEL
 module.exports = User;
--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,27 +1,43 @@
 //pulling in packages
-const router = require('express').Router();
-const User = require('../models/User');
-const Portfolio = require('../models/Portfolio');  
+const router = require("express").Router();
+const User = require("../models/User");
+const Portfolio = require("../models/Portfolio");
 
 //check if user is logged in
 function isAuthenticated(req, res, next) {
-    if (!req.session.user_id)
-      return res.status(401).send({ error: 'You must be logged in' });
-    next();
+  if (!req.session.user_id)
+    return res.status(401).send({ error: "You must be logged in" });
+  next();
 }
 
 //route to get all portfolios
-router.get('/portfolios', isAuthenticated, async (req, res) => {
+router.get("/portfolios", isAuthenticated, async (req, res) => {
   //find all portfolios and populate user
-  const portfolios = await Portfolio.find({}).populate('user');
+  const portfolios = await Portfolio.find({}).populate("user");
   //return json of portfolios
   res.send(portfolios);
 });
 
 //route to get a single portfolio
-router.get('/portfolios/:id', isAuthenticated, async (req, res) => {
+router.get("/portfolios/:id", isAuthenticated, async (req, res) => {
   //find portfolio by id and populate user
-  const portfolio = await Portfolio.findById(req.params.id).populate('user');
+  const portfolio = await Portfolio.findById(req.params.id).populate("user");
+  //return json of portfolio
+  res.send({ portfolio: portfolio });
+});
+
+//route to create a portfolio
+router.post("/portfolios", isAuthenticated, async (req, res) => {
+  //create new portfolio
+  const portfolio = new Portfolio(req.body);
+  //save portfolio
+  await portfolio.save();
+  //find user
+  const user = await User.findById(req.session.user_id);
+  //update user to include newly created portfolio
+  user.portfolios.push(portfolio._id);
+  //save updated user
+  await user.save();
   //return json of portfolio
   res.send({ portfolio: portfolio });
 });
