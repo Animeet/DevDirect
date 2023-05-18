dec3417b51d75d9d4475834804b81f1369c4b0cd
Zachary Barnes <zachhsavage@gmail.com>
2023-05-18 15:05:04 -0400

    connection js change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,11 @@
-//pulling in mongoose
 const mongoose = require('mongoose');
 
-//pulling in PORT 
-const PORT = process.env.PORT;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/devdirect_db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
-//connecting to db
-mongoose.connect(PORT ? process.env.DB_URL : 'mongodb://127.0.0.1:27017/devdirect_db');
-
-//exporting mongoose
 module.exports = mongoose.connection;
-
