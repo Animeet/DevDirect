d2c746495b81f41aec83d0f1e7af75e7bb15f0a8
Zachary Barnes <zachhsavage@gmail.com>
2023-05-18 11:03:12 -0400

    Foundation for filter fix

--- client/src/pages/Prospect.js
+++ client/src/pages/Prospect.js
@@ -2,23 +2,45 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function Prospect() {
+function Prospect(props) {
   const [portfolios, setPortfolios] = useState([]);
+  const [filteredPortfolios, setFilteredPortfolios] = useState([]);
 
   // function to check if user is logged in
   useEffect(() => {
     axios.get(`/api/portfolios/all`, { withCredentials: true }).then((res) => {
       console.log(res.data);
-      setPortfolios(res.data);
+      if (props.user) {
+        const filtered = res.data.map((portfolio) => {
+          return {
+            ...portfolio,
+          };
+        });
+        setPortfolios(filtered);
+      } else setPortfolios(res.data);
     });
   }, []);
+
+  const searchPortfolios = (e) => {
+    const input = e.target.value;
+    if (input.length) {
+      const filtered = portfolios.filter((portfolio) => {
+        return portfolio?.user?.first_name
+          ?.toLowerCase()
+          .includes(input.toLowerCase());
+      });
+      console.log(filtered);
+      setFilteredPortfolios(filtered);
+    } else setFilteredPortfolios([]);
+  };
+
   return (
     <div>
       <section class="flex justify-center registerbody prospectpage">
         <div class="flex flex-col m-8">
           <div class="flex justify-center pb-10">
             <input
-              onChange=""
+              onChange={searchPortfolios}
               class="text-center border-2 border-gray-300 bg-white h-10 p-6 rounded-lg text-lg focus:outline-none"
               type="text"
               placeholder="Search Prospects"
@@ -26,25 +48,29 @@ function Prospect() {
           </div>
 
           <div class="grid-forms">
-            {portfolios.map((portfolio) => {
-              return (
-                <a href={`/profile/${portfolio._id}`}>
-                  <div class="formdimension bg-white p-3 rounded-lg">
-                    <p class="pb-2 text-red-500">{portfolio.languages}</p>
-                    <hr />
-                    <p class="text-center text-4xl p-8 text-bold">
-                      {portfolio?.user?.first_name + " " + portfolio?.user?.last_name}
-                    </p>
-                    <hr />
-                    <div class="flex flex-row justify-center pt-2">
-                      <p class="text-center font-medium">
-                      {portfolio.university}
-                      </p>
-                    </div>
-                  </div>
-                </a>
-              );
-            })}
+            {filteredPortfolios.length
+              ? filteredPortfolios.map(portfolios)
+              : portfolios.map((portfolio) => {
+                  return (
+                    <a href={`/profile/${portfolio._id}`}>
+                      <div class="formdimension bg-white p-3 rounded-lg">
+                        <p class="pb-2 text-red-500">{portfolio.languages}</p>
+                        <hr />
+                        <p class="text-center text-4xl p-8 text-bold">
+                          {portfolio?.user?.first_name +
+                            " " +
+                            portfolio?.user?.last_name}
+                        </p>
+                        <hr />
+                        <div class="flex flex-row justify-center pt-2">
+                          <p class="text-center font-medium">
+                            {portfolio.university}
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                  );
+                })}
           </div>
         </div>
       </section>
@@ -53,3 +79,4 @@ function Prospect() {
 }
 
 export default Prospect;
+
