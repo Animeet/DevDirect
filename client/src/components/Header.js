71f3c2bd39ed0a6d5bd758037aa95759e8602a7d
jamessciacca <noing2003@gmail.com>
2023-05-18 13:13:38 -0400

    ccs edits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -25,11 +25,11 @@ function Header({ user }) {
   return (
     <header>
       <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl" id="nav-div">
           <a href="/" class="flex items-center">
             <img src={logo} class="mr-3 h-14" alt="devDirect Logo" />
           </a>
-          <div class="flex items-center lg:order-2">
+          <div class="flex justify-end items-center lg:order-2" id="btn-div">
             <a
               href="/login"
               class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
@@ -47,7 +47,7 @@ function Header({ user }) {
             ) : (
               <a
                 href="/register"
-                class="text-white bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                class="text-white bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" id="get-started"
               >
                 Get started
               </a>
@@ -123,12 +123,12 @@ function Header({ user }) {
                   href="/prospect"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
-                  Prospect
+                  Prospects
                 </a>
               </li>
             </ul>
           </div>
-          <section className="MOBILE-MENU flex lg:hidden">
+          <section className="MOBILE-MENU flex lg:hidden" id="burger-menu">
             <div
               className="HAMBURGER-ICON space-y-2"
               onClick={() => setIsNavOpen((prev) => !prev)}
