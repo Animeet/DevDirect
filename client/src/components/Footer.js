51abc56b703e4c73288c2b0eb91b1caba79fa206
jamessciacca <noing2003@gmail.com>
2023-05-18 13:21:19 -0400

    created aboutjs, connected the route, and linked the baout button to open the aout.js route

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -19,7 +19,7 @@ function Footer() {
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                         <li>
-                            <a href="/" className="mr-4 hover:underline md:mr-6 ">About</a>
+                            <a href="/about" className="mr-4 hover:underline md:mr-6 ">About</a>
                         </li>
                         <li>
                             <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
---
+++ client/src/pages/About.js
@@ -0,0 +1,7 @@
+function About () {
+    return (
+        <h1>Here is the About Page!</h1>
+    )
+}
+
+export default About;
