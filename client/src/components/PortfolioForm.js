989c5bb771e629bf21b8a7c84e5482273fbee6ff
Zachary Barnes <zachhsavage@gmail.com>
2023-05-17 22:34:39 -0400

    Stylized the dashboard

--- client/src/components/PortfolioForm.js
+++ client/src/components/PortfolioForm.js
@@ -50,194 +50,185 @@ function PortfolioForm(props) {
       }
 
       props.setShowForm(false);
-      // props.setUser(res.data.user);
-
-      // setFormData({
-      //   portfolioLink: "",
-      //   resumeLink: "",
-      //   bio: "",
-      //   githubLink: "",
-      //   linkedInLink: "",
-      //   university: "",
-      //   bootcamp: "",
-      //   languages: "",
-      // });
     } catch (err) {
       setErrorMessage(err.response.data.error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {errorMessage && <p className="error">{errorMessage}</p>}
-      <div className=" flex flex-row justify-between p-6">
-        <label
-          for="portfolioLink"
-          className="font-medium text-gray-900 underline"
-        >
-          Portfolio:
-        </label>
-        <input
-          type="text"
-          name="portfolioLink"
-          id="portfolioLink"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="Portfolio URL"
-          value={formData.portfolioLink}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+    <div className="w-1/2 bg-white m-14 rounded-xl boxyshadow">
+      <form onSubmit={handleSubmit}>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="portfolioLink"
+            className="font-medium text-gray-900 underline"
+          >
+            Portfolio:
+          </label>
+          <input
+            type="text"
+            name="portfolioLink"
+            id="portfolioLink"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="Portfolio URL"
+            value={formData.portfolioLink}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="resumeLink" className="font-medium text-gray-900 underline">
-          Resume:
-        </label>
-        <input
-          type="text"
-          name="resumeLink"
-          id="resumeLink"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="Resume URL"
-          value={formData.resumeLink}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="resumeLink"
+            className="font-medium text-gray-900 underline"
+          >
+            Resume:
+          </label>
+          <input
+            type="text"
+            name="resumeLink"
+            id="resumeLink"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="Resume URL"
+            value={formData.resumeLink}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="bio" class="font-medium text-gray-900 underline">
-          Bio:
-        </label>
-        <textarea
-          name="bio"
-          onChange={handleInputChange}
-          value={formData.bio}
-          id="message"
-          rows="6"
-          class="block p-2.5 w-3/4 text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-          placeholder="Leave a comment..."
-        ></textarea>
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label for="bio" class="font-medium text-gray-900 underline">
+            Bio:
+          </label>
+          <textarea
+            name="bio"
+            onChange={handleInputChange}
+            value={formData.bio}
+            id="message"
+            rows="6"
+            class="block p-2.5 w-3/4 text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+            placeholder="Leave a comment..."
+          ></textarea>
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="languages" className="font-medium text-gray-900 underline">
-          Languages:
-        </label>
-        <input
-          type="text"
-          name="languages"
-          id="languages"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="Languages Learned"
-          value={formData.languages}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="languages"
+            className="font-medium text-gray-900 underline"
+          >
+            Languages:
+          </label>
+          <input
+            type="text"
+            name="languages"
+            id="languages"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="Languages Learned"
+            value={formData.languages}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label
-          for="linkedInLink"
-          className="font-medium text-gray-900 underline"
-        >
-          LinkedIn:
-        </label>
-        <input
-          type="text"
-          name="linkedInLink"
-          id="linkedInLink"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="LinkedIn URL"
-          value={formData.linkedInLink}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="linkedInLink"
+            className="font-medium text-gray-900 underline"
+          >
+            LinkedIn:
+          </label>
+          <input
+            type="text"
+            name="linkedInLink"
+            id="linkedInLink"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="LinkedIn URL"
+            value={formData.linkedInLink}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="githubLink" className="font-medium text-gray-900 underline">
-          Github:
-        </label>
-        <input
-          type="text"
-          name="githubLink"
-          id="githubLink"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="Github URL"
-          value={formData.githubLink}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="githubLink"
+            className="font-medium text-gray-900 underline"
+          >
+            Github:
+          </label>
+          <input
+            type="text"
+            name="githubLink"
+            id="githubLink"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="Github URL"
+            value={formData.githubLink}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="university" className="font-medium text-gray-900 underline">
-          University:
-        </label>
-        <input
-          type="text"
-          name="university"
-          id="university"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="University Name"
-          value={formData.university}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label
+            for="university"
+            className="font-medium text-gray-900 underline"
+          >
+            University:
+          </label>
+          <input
+            type="text"
+            name="university"
+            id="university"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="University Name"
+            value={formData.university}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
+        <hr />
 
-      <div className=" flex flex-row justify-between p-6">
-        <label for="bootcamp" className="font-medium text-gray-900 underline">
-          Bootcamp:
-        </label>
-        <input
-          type="text"
-          name="bootcamp"
-          id="bootcamp"
-          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-          placeholder="Bootcamp Name"
-          value={formData.bootcamp}
-          onChange={handleInputChange}
-          required=""
-        />
-      </div>
+        <div className=" flex flex-row justify-between p-6">
+          <label for="bootcamp" className="font-medium text-gray-900 underline">
+            Bootcamp:
+          </label>
+          <input
+            type="text"
+            name="bootcamp"
+            id="bootcamp"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
+            placeholder="Bootcamp Name"
+            value={formData.bootcamp}
+            onChange={handleInputChange}
+            required=""
+          />
+        </div>
 
-      <hr />
-      <div className="flex justify-center">
-        <button
-          type="submit"
-          className="w-1/4 m-4 text-white bg-sky-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        >
-          {props.formDetails.isUpdate ? "Update" : "Save"}
-        </button>
-      </div>
-
-      {/* <div>
-        <h4>Portfolio: {formData.portfolioLink}</h4>
-        <h4>Resume: {formData.resumeLink}</h4>
-        <h4>Bio: {formData.bio}</h4>
-        <h4>GitHub: {formData.githubLink}</h4>
-        <h4>LinkedIn: {formData.linkedInLink}</h4>
-        <h4>University: {formData.university}</h4>
-        <h4>Bootcamp: {formData.bootcamp}</h4>
-        <h4>Languages: {formData.languages}</h4>
-      </div> */}
-    </form>
+        <hr />
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="w-1/4 m-4 text-white bg-sky-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          >
+            {props.formDetails.isUpdate ? "Update" : "Save"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -9,12 +9,14 @@ function Dashboard() {
     isUpdate: false,
     id: "",
   });
+  const [portfolioCreated, setPortfolioCreated] = useState(false);
 
   useEffect(() => {
     axios.get("/api/portfolios").then((res) => {
       setPortfolios(res.data);
+      setPortfolioCreated(res.data.length > 0);
     });
-  }, [portfolios]);
+  }, []);
 
   const handlePortfolioUpdate = (id) => {
     const portfolio = portfolios.find((p) => p._id === id);
@@ -26,130 +28,138 @@ function Dashboard() {
     setShowForm(true);
   };
 
+  const handlePortfolioCreated = () => {
+    setShowForm(false);
+    setPortfolioCreated(true);
+  };
+
+  const handleAddPortfolioClick = () => {
+    setShowForm(true);
+  };
+
   return (
-    <>
+    <div className="registerbody flex justify-center">
       {showForm && (
         <PortfolioForm
           formDetails={formDetails}
           setShowForm={setShowForm}
           portfolios={portfolios}
           setPortfolios={setPortfolios}
+          onPortfolioCreated={handlePortfolioCreated}
         />
       )}
-      {
-        portfolios.length ? null : <button onClick={() => setShowForm(true)}>Add Portfolio</button>
-      }
+      {!portfolioCreated && !showForm && portfolios.length === 0 && (
+        <div className="flex justify-center addportfolio">
+          <div className="h-1/2 bg-gray-100 align-middle p-4 portfolioform rounded-xl">
+            <h1 className="text-2xl font-bold text-center py-8">
+              Create Your Portfolio
+            </h1>
+            <button
+              className="bg-sky-400 text-3xl text-white px-6 py-3 rounded-xl m-10"
+              onClick={handleAddPortfolioClick}
+            >
+              Add Portfolio
+            </button>
+          </div>
+        </div>
+      )}
       {portfolios.map((portfolio) => {
         return (
           <div key={portfolio._id}>
-            <h2>{portfolio.bio}</h2>
-            <button onClick={() => handlePortfolioUpdate(portfolio._id)}>
-              Update
-            </button>
-            <h2>{portfolio.portfolioLink}</h2>
-            <h2>{portfolio.resumeLink}</h2>
-            <h2>{portfolio.githubLink}</h2>
-            <h2>{portfolio.linkedinLink}</h2>
-            <h2>{portfolio.university}</h2>
-            <h2>{portfolio.bootcamp}</h2>
-            <h2>{portfolio.languages}</h2>
+            <section class="flex justify-center registerbody profilepage rounded-xl m-12">
+              <div class="m-10 p-8 bg-white rounded-2xl profiledesign boxyshadow">
+                <h1 class="text-8xl text-center p-8 font-medium">
+                  {portfolio?.user?.first_name +
+                    " " +
+                    portfolio?.user?.last_name}
+                </h1>
+
+                <hr />
+
+                <h3 class="text-center text-4xl p-8">{portfolio.university}</h3>
+
+                <div class="flex justify-center">
+                  <hr class="w-3/4" />
+                </div>
+
+                <div class="flex flex-row justify-evenly p-8">
+                  <div class="flex flex-col w-1/2">
+                    <p class="text-center text-4xl underline">Portfolio</p>
+                    <a class="text-center" href="">
+                      {portfolio.portfolioLink}
+                    </a>
+                  </div>
+                  <div class="flex flex-col w-1/2">
+                    <p class="text-center text-4xl underline">Resume</p>
+                    <a class="text-center" href="">
+                      {portfolio.resumeLink}
+                    </a>
+                  </div>
+                </div>
+
+                <div class="flex justify-center">
+                  <hr class="w-3/4" />
+                </div>
+
+                <div class="flex flex-row justify-evenly p-8">
+                  <div class="flex flex-col w-1/2">
+                    <p class="text-center text-4xl underline">Github</p>
+                    <a class="text-center" href="">
+                      {portfolio.githubLink}
+                    </a>
+                  </div>
+                  <div class="flex flex-col w-1/2">
+                    <p class="text-center text-4xl underline">LinkedIn</p>
+                    <a class="text-center" href="">
+                      {portfolio.linkedInLink}
+                    </a>
+                  </div>
+                </div>
+
+                <div class="flex justify-center">
+                  <hr class="w-3/4" />
+                </div>
+
+                <div>
+                  <h3 class="text-center text-4xl p-8">
+                    Programming Languages
+                  </h3>
+                  <div class="flex flex-row justify-evenly pb-8">
+                    <h5 class="text-xl">{portfolio.languages}</h5>
+                  </div>
+                </div>
+
+                <div class="flex justify-center">
+                  <hr class="w-3/4" />
+                </div>
+
+                <div class="flex justify-center p-8">
+                  <a
+                    class="bg-sky-300 px-8 py-3 rounded-lg"
+                    href={`mailto:${portfolio.user.email}`}
+                  >
+                    Contact Via Email
+                  </a>
+                </div>
+              </div>
+            </section>
+
+            {/* <h2>{portfolio.bio}</h2> */}
+            <div className="flex flex-row justify-center">
+              <button
+                className="bg-white text-2xl font-medium px-8 py-3 mx-8 rounded-lg mb-10 boxyshadow"
+                onClick={() => handlePortfolioUpdate(portfolio._id)}
+              >
+                Update
+              </button>
+
+            <button className="bg-red-500 text-2xl font-medium px-8 py-3 mx-8 rounded-lg mb-10 boxyshadow">DELETE</button>
+            </div>
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
 
 export default Dashboard;
-
-{
-  /* <div>
-            <section className="registerbody dashform">
-                <div className="dashsize flex flex-col m-12">
-                    <h1 className="text-center p-10 font-bold text-5xl tracking-wider">Student Dashboard</h1>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="portfolio" className="font-medium text-gray-900 underline">Portfolio:</label>
-                        <input type="text" name="portfolio" id="portfolio"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="Portfolio URL" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="resume" className="font-medium text-gray-900 underline">Resume:</label>
-                        <input type="text" name="resume" id="resume"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="Resume URL" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="bio" class="font-medium text-gray-900 underline">Bio:</label>
-                        <textarea
-                            id="message"
-                            rows="6"
-                            class="block p-2.5 w-3/4 text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-                            placeholder="Leave a comment..."
-                        ></textarea>
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="languages" className="font-medium text-gray-900 underline">Languages:</label>
-                        <input type="text" name="languages" id="languages"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="Languages Learned" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="linkedin" className="font-medium text-gray-900 underline">LinkedIn:</label>
-                        <input type="text" name="linkedin" id="linkedin"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="LinkedIn URL" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="github" className="font-medium text-gray-900 underline">Github:</label>
-                        <input type="text" name="github" id="github"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="Github URL" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="university" className="font-medium text-gray-900 underline">University:</label>
-                        <input type="text" name="university" id="university"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="University Name" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className=" flex flex-row justify-between p-6">
-                        <label for="bootcamp" className="font-medium text-gray-900 underline">Bootcamp:</label>
-                        <input type="text" name="bootcamp" id="bootcamp"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block p-2 w-3/4"
-                            placeholder="Bootcamp Name" required="" />
-                    </div>
-
-                    <hr />
-
-                    <div className="flex justify-center">
-                        <button type="submit"
-                            className="w-1/4 m-4 text-white bg-sky-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Save</button>
-                    </div>
-                </div>
-            </section>
-        </div> */
-}
