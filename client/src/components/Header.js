758ff273cc506aba7326fb51d81202671ecb9b53
Zachary Barnes <zachhsavage@gmail.com>
2023-05-18 22:12:37 -0400

    creating a commit to make sure we are up to date

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -42,7 +42,7 @@ function Header({ user }) {
               <a
                 onClick={logOutHandler}
                 href="#"
-                class="text-white bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                class="text-white bg-blue-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 focus:outline-none"
               >
                 Log Out
               </a>
